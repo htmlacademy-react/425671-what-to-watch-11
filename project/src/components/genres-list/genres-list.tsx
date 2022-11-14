679983c56d26c1b93b5d201dab4373db5f74571a
pt1c <ravenzone@mail.ru>
2022-11-14 10:18:27 +0300

    typo fix

--- project/src/components/genres-list/genres-list.tsx
+++ project/src/components/genres-list/genres-list.tsx
@@ -9,7 +9,7 @@ export default function GenresList(): JSX.Element {
 
   const getListItem = (genreName: string, active: boolean) => (
     <li key={`genre-${genreName}`}className={active ? 'catalog__genres-item catalog__genres-item--active' : 'catalog__genres-item'}>
-      <a href="\#" className="catalog__genres-link" onClick={() => genreName === DEFAULT_GENRE ? dispatch(genreReset()) : dispatch(genreSet(genreName))}>{genreName}</a>
+      <a href="/#" className="catalog__genres-link" onClick={() => genreName === DEFAULT_GENRE ? dispatch(genreReset()) : dispatch(genreSet(genreName))}>{genreName}</a>
     </li>
   );
 
